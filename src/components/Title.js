b17f899b4d4d2fb54de69a049d7f17dc96ffaa36
bio8oid <8bollod8@mail.co.uk>
2019-06-10 19:36:38 +0200

    Add Title component

---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from './Title.css';
+
+const Title = props => <h1>{props.title}</h1>
+
+			<div className={style.TitleApp}>
+                ToDo
+            </div>
+
+
+export default Title;
